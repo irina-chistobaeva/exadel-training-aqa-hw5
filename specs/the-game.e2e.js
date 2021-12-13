bf3a9447d265231283af1e979153e8a3ae83f20c
Iryna Chystabayeva <chistobaevairina@gmail.com>
2021-12-13 18:39:29 +0300

    Add hw #5 task 4

--- specs/the-game.e2e.js
+++ specs/the-game.e2e.js
@@ -1,13 +1,29 @@
 describe('The game', function () {
     it('should get more than 100 points ', async function () {
-        await browser.url('http://localhost:63342/course-sut/arkanoid.html');
+        await browser.url('https://viktor-silakov.github.io/course-sut/arkanoid.html');
         await browser.pause(1000);
         await $('button=PLAY').click();
         await browser.waitUntil(async () => {
-            // player logic
+            let ballLocation;
+            let padLocation;
+
+            do {
+                ballLocation = await $('#ball').getLocation('x');
+                padLocation = await $('#pad').getLocation('x');
+                await browser.keys(['D']);
+            } while (ballLocation > padLocation);
+
+            do {
+                ballLocation = await $('#ball').getLocation('x');
+                padLocation = await $('#pad').getLocation('x');
+                await browser.keys(['A']);
+            } while (ballLocation < padLocation);
+
+            console.log(ballLocation);
+            console.log(padLocation);
             const points = parseInt(await $('#points').getText(), 10);
             if (points > 100) return true
             console.log({ points })
-        }, { timeout: 600000, interval: 10 })
+        }, { timeout: 60000, interval: 10 })
     });
 })
